99cc6ef44daf0404ec7ba3bc5887f2b51d80deb9
Deepanshu <d4deepanshu723@gmail.com>
2025-02-11 01:28:44 +0530

    edge

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import { authRouter } from "./routes/user";
 import { characterRouter } from "./routes/character";
+import { handle } from "hono/vercel";
 
-const app = new Hono().basePath("/api").route("/auth", authRouter).route("/character",characterRouter);
+export const runtime = 'edge'
+
+const app = new Hono().basePath("/api").route("/auth", authRouter).route("/character", characterRouter);
 
 app.get("/health", (c) => {
   return c.json({
@@ -11,4 +14,13 @@ app.get("/health", (c) => {
 })
 
 
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const PATCH = handle(app);
+export const OPTIONS = handle(app);
+export const HEAD = handle(app);
+
 export default app;
+
