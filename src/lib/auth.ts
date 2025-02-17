536db070e7d7ff4db322addcfc2da43b03a56005
Deepanshu <d4deepanshu723@gmail.com>
2025-02-17 21:09:28 +0530

    change accepted

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,19 +12,25 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }
-  }, 
-  secret:process.env.BETTER_AUTH_SECRET as string,
-  advanced:{
-    defaultCookieAttributes:{
-      sameSite:"none",
-      secure:true,
-      httpOnly:true,
+  },
+  secret: process.env.BETTER_AUTH_SECRET as string,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+    crossSubDomainCookies: {
+      enabled: true,
+
     }
   },
   trustedOrigins: [
     "https://holo-ai-one.vercel.app",
     "https://chat-backend-vve1.onrender.com",
     "http://localhost:3001",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://holo.deepanshumishra.me",
+    "https://api-holo.deepanshumishra.me"
   ]
 });
